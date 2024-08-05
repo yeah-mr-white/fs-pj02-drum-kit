c64f1d6bf623e4cf967459d3bda035e8876003d9
yeah-mr-white <eyeforparrot78145@gmail.com>
2024-08-05 19:42:30 +0700

    work in progress

--- app.js
+++ app.js
@@ -1,47 +1,65 @@
 let numOfBtn = document.querySelectorAll('.drum').length;
 
 for (let i = 0; i < numOfBtn; i++) {
-    document.querySelectorAll('.drum')[i].addEventListener('keyup', function (event) {
-        let char = event.key;
+    document
+        .querySelectorAll('.drum')
+        [i].addEventListener('keyup', function (event) {
+            let char = event.key;
 
-        switch (char) {
-            case 'w':
-                let w = new Audio('sounds/crash.mp3');
-                w.play();
-                break;
+            switch (char) {
+                case 'w':
+                    let w = new Audio('sounds/crash.mp3');
+                    w.play();
+                    break;
 
-            case 'a':
-                let a = new Audio('sounds/kick-bass.mp3');
-                a.play();
-                break;
+                case 'a':
+                    let a = new Audio('sounds/kick-bass.mp3');
+                    a.play();
+                    break;
 
-            case 's':
-                let s = new Audio('sounds/snare.mp3');
-                s.play();
-                break;
+                case 's':
+                    let s = new Audio('sounds/snare.mp3');
+                    s.play();
+                    break;
 
-            case 'd':
-                let d = new Audio('sounds/tom-1.mp3');
-                d.play();
-                break;
+                case 'd':
+                    let d = new Audio('sounds/tom-1.mp3');
+                    d.play();
+                    break;
 
-            case 'j':
-                let j = new Audio('sounds/tom-2.mp3');
-                j.play();
-                break;
+                case 'j':
+                    let j = new Audio('sounds/tom-2.mp3');
+                    j.play();
+                    break;
 
-            case 'k':
-                let k = new Audio('sounds/tom-3.mp3');
-                k.play();
-                break;
+                case 'k':
+                    let k = new Audio('sounds/tom-3.mp3');
+                    k.play();
+                    break;
 
-            case 'l':
-                let l = new Audio('sounds/tom-4.mp3');
-                l.play();
-                break;
+                case 'l':
+                    let l = new Audio('sounds/tom-4.mp3');
+                    l.play();
+                    break;
 
-            default:
-                char;
-        }
-    });
+                default:
+                    char;
+            }
+        });
 }
+
+let obj1 = {
+    name: 'Aung Aung',
+    age: 34,
+    marital_status: false,
+    foreign_exp: false,
+};
+
+let obj2 = {
+    name: 'Su Su',
+    age: 25,
+    marital_status: true,
+    foreign_exp: true,
+};
+
+console.log(obj1.name);
